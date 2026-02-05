f9ff5151cd556a9681f9cc884567f63354c4c3c1
Cheena <cheenagarg014@ggmail.com>
2026-02-05 12:22:50 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let name="chee";//string
+let age=10;//number
+let boolean=true;//boolean-true,false
+//null-standalone value-empty
+//undefined=not value assign;
+//symbol-to find uniquness
+
+
+//Object
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
