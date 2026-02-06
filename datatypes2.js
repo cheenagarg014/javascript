26240fba8280a356651d36c2b20cfab9f529defd
Cheena <cheenagarg014@ggmail.com>
2026-02-06 14:28:17 +0530

    datatypes_part2

---
+++ datatypes2.js
@@ -0,0 +1,18 @@
+//Primitive-call by value
+//7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+//Refernce Type-Non Primitive
+//Array,Objects,Functions
+let arr=[12,"chee","dhgfe"]
+
+let obj={
+    name:"cbdc",
+    age:2,
+}
+
+
+ let fun=function (){
+    console.log("fun kre"); 
+}
+
+fun();
